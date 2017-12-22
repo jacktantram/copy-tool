1da18e4946771a7761ad872d8083db0666b80100
Jack tantram <jackt08@hotmail.co.uk>
2017-12-22 11:44:12 +0000

    Adding basic implementation for train tomorrow

--- background_script.py
+++ background_script.py
@@ -1,63 +1,17 @@
-import pyxhook
-import time
+import keyboard
 import pyperclip
 
-clipboard = []
 
-control_pressed = False
-alt_pressed = False
-shift_pressed = False
-current_position = 0
-# This function is called every time a key is presssed
-def kbevent(event):
-    global control_pressed
-    global  clipboard
-    global alt_pressed
-    global shift_pressed
-    global current_position
-    # print key info
-    print  event.Ascii
+clipboard = set()
 
-    # If the ascii value matches spacebar, terminate the while loop
-    if event.Ascii == 227 or control_pressed:
-        control_pressed = True
-        if event.Ascii == 99:
-            print "Ctrl C"
-            clipboard.append(pyperclip.paste())
-            current_position = len(clipboard)-1
-    else:
-        control_pressed = False
+def on_press(key):
+    print key
+    if keyboard.is_pressed('ctrl+c'):
+       clipboard.add(pyperclip.paste())
+       print clipboard
+    if keyboard.is_pressed('ctrl+shift+left'):
+        print "LEFT"
 
-    if event.Ascii == 225 or shift_pressed:
-        shift_pressed = True
-        if event.Ascii == 233 or alt_pressed:
-            alt_pressed = True
+keyboard.on_press(on_press)
 
-            if event.Ascii == 81:
-                print clipboard[current_position]
-                current_position-=1
-
-
-        else:
-            alt_pressed = False
-
-    else:
-        shift_pressed = False
-
-
-# Create hookmanager
-hookman = pyxhook.HookManager()
-# Define our callback to fire when a key is pressed down
-hookman.KeyDown = kbevent
-# Hook the keyboard
-hookman.HookKeyboard()
-# Start our listener
-hookman.start()
-
-# Create a loop to keep the application running
-running = True
-while running:
-    time.sleep(0.1)
-
-# Close the listener when we are done
-hookman.cancel()
+keyboard.wait()
